a78477a1840a3ecd1bcb3e3cf78bbfaa1ccc8d2a
Alessandro Re <ale@ale-re.net>
2026-05-24 11:24:30 +0200

    feat: nix workflow

--- flake.nix
+++ flake.nix
@@ -4,22 +4,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+
+    sagome.url = "github:pigreco-luogoideale/sagome";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          packageName = "pigreco-luogoideale-website";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          packages.default = pkgs.zola;
-          devShells.default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.zola
-              pkgs.python39Packages.weasyprint
-            ];
-          };
-        });
+    (system: let
+      packageName = "pigreco-luogoideale-website";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.zola;
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.zola
+          pkgs.python39Packages.weasyprint
+        ];
+      };
+    });
 }
